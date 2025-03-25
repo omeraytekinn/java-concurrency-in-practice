4e186fe5fd2cff44917dcbf02b79b852c2b8e77a
Ömer Aytekin <o.niketya@gmail.com>
2025-03-25 03:47:42 +0300

    Added servlets to tomcat

--- ch2-thread-safety/example-1/src/main/java/com/omeraytekin/thread_safety/Main.java
+++ ch2-thread-safety/example-1/src/main/java/com/omeraytekin/thread_safety/Main.java
@@ -6,7 +6,10 @@ import org.apache.catalina.Context;
 import org.apache.catalina.LifecycleException;
 import org.apache.catalina.startup.Tomcat;
 
+import com.omeraytekin.thread_safety.servlets.CountingFactorizer;
 import com.omeraytekin.thread_safety.servlets.StatelessFactorizer;
+import com.omeraytekin.thread_safety.servlets.SynchronizedFactorizer;
+import com.omeraytekin.thread_safety.servlets.UnsafeCachingFactorizer;
 import com.omeraytekin.thread_safety.servlets.UnsafeCountingFactorizer;
 
 import jakarta.servlet.GenericServlet;
@@ -22,16 +25,31 @@ public class Main {
 
         Context context = tomcat.addContext(contextPath, docBase);
 
-        GenericServlet servletSF = new StatelessFactorizer();
-        GenericServlet servletUCF = new UnsafeCountingFactorizer();
-        String servletNameSF = "StatelessFactorizer";
-        String urlPatternSF = "/stateless-factorizer";
-        String servletNameUCF = "UnsafeCountingFactorizer";
-        String urlPatternUCF = "/unsafe-counting-factorizer";
-        tomcat.addServlet(contextPath, servletNameSF, servletSF);
-        tomcat.addServlet(contextPath, servletNameUCF, servletUCF);
-        context.addServletMappingDecoded(urlPatternSF, servletNameSF);
-        context.addServletMappingDecoded(urlPatternUCF, servletNameUCF);
+        GenericServlet servletCF = new CountingFactorizer();
+        GenericServlet servletStlF = new StatelessFactorizer();
+        GenericServlet servletSyncF = new SynchronizedFactorizer();
+        GenericServlet servletUCachF = new UnsafeCachingFactorizer();
+        GenericServlet servletUCntF = new UnsafeCountingFactorizer();
+        String servletNameCF = "CountingFactorizer";
+        String servletNameStlF = "StatelessFactorizer";
+        String servletNameSyncF = "SynchronizedFactorizer";
+        String servletNameUCachF = "UnsafeCachingFactorizer";
+        String servletNameUCntF = "UnsafeCountingFactorizer";
+        String urlPatternCF = "/counting-factorizer";
+        String urlPatternStlF = "/stateless-factorizer";
+        String urlPatternSync = "/synchronized-factorizer";
+        String urlPatternUCachF = "/unsafe-caching-factorizer";
+        String urlPatternUCntF = "/unsafe-counting-factorizer";
+        tomcat.addServlet(contextPath, servletNameCF, servletCF);
+        tomcat.addServlet(contextPath, servletNameStlF, servletStlF);
+        tomcat.addServlet(contextPath, servletNameSyncF, servletSyncF);
+        tomcat.addServlet(contextPath, servletNameUCachF, servletUCachF);
+        tomcat.addServlet(contextPath, servletNameUCntF, servletUCntF);
+        context.addServletMappingDecoded(urlPatternCF, servletNameCF);
+        context.addServletMappingDecoded(urlPatternStlF, servletNameStlF);
+        context.addServletMappingDecoded(urlPatternSync, servletNameSyncF);
+        context.addServletMappingDecoded(urlPatternUCachF, servletNameUCachF);
+        context.addServletMappingDecoded(urlPatternUCntF, servletNameUCntF);
         tomcat.start();
         tomcat.getServer().await();
     }
